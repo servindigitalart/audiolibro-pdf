44dcbd21ade8d0a17d0a52fa1ee1871f86fab18b
Servin Emilio <servindigitalart@gmail.com>
2026-05-14 14:44:15 -0600

    fix vite alias resolution for vercel

--- sonoro/astro.config.mjs
+++ sonoro/astro.config.mjs
@@ -2,14 +2,25 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import path from 'path';
 
 export default defineConfig({
   output: 'server',
+
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
   ],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
